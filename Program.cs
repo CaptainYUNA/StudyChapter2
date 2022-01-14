53a41bde3ab2a96d8994aecd26b23a859bd17a69
CaptainYUNA <97616908+CaptainYUNA@users.noreply.github.com>
2022-01-14 17:01:37 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -47,12 +47,16 @@ namespace StudyChapter2
             //Study.Power(2, 20); //Cahpter 77
             //Study.AreaOfCircle(10);
 
-            //Study.CalculateAge(1992, 4, 27);
-            var powerResult = StudyRecurison.RecurisonPower(5, 5);
-            Console.WriteLine($"Result of Recurison Power Method: {powerResult}");
+            Study.CalculateAge(1992, 4, 27);
 
-            var factorialResult = StudyRecurison.RecurisonFactorial(10);
-            Console.WriteLine($"Result of Recurison Power Method: {factorialResult}");
+
+            Study.TestCalculateAge(new DateTime(1992, 4, 27), DateTime.Today);
+            //Study.TestCalculateAge(new DateTime(1992, 4, 27), DateTime.Today.AddMonths(1));
+            //var powerResult = StudyRecurison.RecurisonPower(5, 5);
+            //Console.WriteLine($"Result of Recurison Power Method: {powerResult}");
+
+            //var factorialResult = StudyRecurison.RecurisonFactorial(10);
+            //Console.WriteLine($"Result of Recurison Power Method: {factorialResult}");
 
         }
 
